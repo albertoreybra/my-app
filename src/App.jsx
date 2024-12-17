36ba8b8cdede6d500704f628140ad1fe7c5ace60
albertoreybra <90837015+albertoreybra@users.noreply.github.com>
2024-12-17 17:23:22 +0100

    Modificado archivo App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import './App.css';
 import Characters from './components/Characters';
+import Favorites from './components/Favorites';
 import imageRickMorty from './img/rick-morty.png';
 import { useState } from 'react';
 
 function App() {
 
   const [characters, setCharacters] = useState(null);
-
+  const [favorites, setFavorites] = useState([]);
 
   const reqAPI = async () => {
     const api = await fetch('https://rickandmortyapi.com/api/character');
     const characterAPI = await api.json(); // Obtener el JSON de la petición HTTP
     setCharacters(characterAPI.results);
+  }
+
+  function toggleFavorite(character) {
+    const isFavorite = favorites.includes(character);
+    if (isFavorite) {
+      const newFavorites = favorites.filter(fav => fav !== character);
+      setFavorites(newFavorites);
+    } else {
+      setFavorites([...favorites, character]);
+    }
   }
 
 
@@ -20,7 +31,10 @@ function App() {
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
         {characters ? (
-          <Characters characters={characters} setCharacters={setCharacters}/>
+          <>
+            <Characters characters={characters} setCharacters={setCharacters} favorites={favorites} toggleFavorite={toggleFavorite}/>
+            <Favorites favorites={favorites} toggleFavorite={toggleFavorite}/>
+          </>
         ) : (
           <>
             <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
